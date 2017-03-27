9211afd41603831f2d8bd9a731d163e5a12869f8
Pranay Maharana <pranay.maharana@gmail.com>
2017-03-27 18:59:27 -0400

    --added bank and house

--- TheTown/TheTown/Bank.cs
+++ TheTown/TheTown/Bank.cs
@@ -6,7 +6,66 @@ using System.Threading.Tasks;
 
 namespace TheTown
 {
-    class Bank
+    public class Bank
     {
+        public string Name { get; set; }
+        public string BuildingType { get; set; }
+        public int NumberOfROom { get; set; }
+        public string ConstructionMaterial { get; set; }
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public double Length { get; set; }
+        public double MyProperty { get; set; }
+        public double SquareFootage
+        {
+            get { return this.Width * this.Length; }
+
+        }
+        public int Floors { get; set; }
+        public int Classrooms { get; set; }
+        public int BathRooms { get; set; }
+        public double Volume
+        {
+            get { return this.Width * this.Length * this.Height; }
+
+        }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public string FullAddress
+        {
+            get { return Address1 + Address2 + City + State + ZipCode; }
+
+        }
+        public int Occupants { get; set; }
+
+        public void IncrementOccupants()
+        {
+            Occupants++;
+        }
+        public void DecrementOccupants()
+        {
+            Occupants--;
+        }
+        private double _amountofmoney;
+
+        public double AmountOfMoney
+        {
+            get { return _amountofmoney; }
+            
+        }
+
+        public void WithdrawMoney(double withdrawmoney)
+        {
+            withdrawmoney += _amountofmoney;
+        }
+
+        public void DepositMoney (double depositmoney)
+        {
+            depositmoney = -_amountofmoney;
+        }
+
     }
 }
