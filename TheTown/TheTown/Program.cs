dfe79d783a2f02292b2d1e7657968e92fa233086
Pranay Maharana <pranay.maharana@gmail.com>
2017-03-27 18:05:30 -0400

    --added school class

---
+++ TheTown/TheTown/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheTown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var mySchool = new School()
+            {
+                Length = 120,
+                Width = 10,
+                Height = 10
+            };
+            var schoolVol = mySchool.SquareFootage;
+            Console.WriteLine($"The total square foot is {schoolVol}");
+        }
+    }
+}
