e74ad4ac0de75372e210270e28234b5f328bdb24
Pranay Maharana <pranay.maharana@gmail.com>
2017-03-27 20:19:17 -0400

    --started level 2, made unique banks, houses, school

--- TheTown/TheTown/Program.cs
+++ TheTown/TheTown/Program.cs
@@ -10,15 +10,48 @@ namespace TheTown
     {
         static void Main(string[] args)
         {
-
-            var mySchool = new School()
+            House house1 = new House()
             {
-                Length = 120,
-                Width = 10,
-                Height = 10
+                BathRooms = 3,
+                BedRooms = 4,
+                Floors = 2
             };
-            var schoolVol = mySchool.SquareFootage;
-            Console.WriteLine($"The total square foot is {schoolVol}");
+            House house2 = new House()
+            {
+                BathRooms = 1,
+                BedRooms = 2,
+                Floors = 1
+            }
+            ;
+            House house3 = new House()
+            {
+                BathRooms = 2,
+                BedRooms = 3,
+                Floors = 1
+            }
+            ;
+            List<House> houseList = new List<House>();
+            house1.Name = "Schweinsteigers";
+            house2.Name = "Maharana";
+            house3.Name = "Boulengaire";
+
+            Bank bank1 = new Bank();
+            Bank bank2 = new Bank();
+            Bank bank3 = new Bank();
+
+            List<Bank> bankList = new List<Bank>();
+
+            School school1 = new School();
+            School school2 = new School();
+            School school3 = new School();
+
+            List<School> schoolList = new List<School>();
+
+
+
+
+
+
         }
     }
 }
